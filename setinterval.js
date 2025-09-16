3549be38d2189c89812758f64316edc69564b25d
Kabz Jose <josekabz00@gmail.com>
2025-09-16 22:51:01 +0300

    Digital clock

--- setinterval.js
+++ setinterval.js
@@ -8,3 +8,8 @@ let timer=setInterval(()=>{
     console.log("stopped")
    }
 },2000)
+
+setInterval(()=>{
+    let now= new Date()
+    console.log(now.toLocaleTimeString())//Digital clock
+},1000)
