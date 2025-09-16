07fbf8bdababea9644a6065abd381a149d70435e
Kabz Jose <josekabz00@gmail.com>
2025-09-16 23:01:13 +0300

    Added code to show value from 10-0 using setinterval

--- setinterval.js
+++ setinterval.js
@@ -13,3 +13,13 @@ setInterval(()=>{
     let now= new Date()
     console.log(now.toLocaleTimeString())//Digital clock
 },1000)
+
+let num =10
+let time=setInterval(()=>{
+    console.log("Count: " +num)//counts from 10-0
+    num --
+   if(num<1){
+    clearInterval(time)
+    console.log("stopped")
+   }
+},2000)
