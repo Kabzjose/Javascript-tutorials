890c86b483187bbb103e1e568717266af9f089ea
Kabz Jose <josekabz00@gmail.com>
2025-09-23 00:01:49 +0300

    Async with fetch displaying an error

--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-let boilRice=new Promise((resolve,reject)=>{
+/*let boilRice=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve("Rice is ready")
     },3000)
@@ -7,9 +7,21 @@ async function cook() {
     console.log("start cooking")
     let rice=await boilRice//waits here
     console.log(rice)//runs after rice is ready
-    
+
     console.log("Food is ready")
     
 }
 console.log("washing dishes")// runs first because its outside the asyn function
 cook()
+*/
+async function name() {
+    try{
+        let res=await fetch("https://wrong-url.com/data")
+        let data=await res.json
+        console.log(data)
+    }catch (error){
+   console.log("Error finding data:",error.message)
+    }
+    
+}
+name()
