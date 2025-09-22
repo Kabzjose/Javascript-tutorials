4937b7149f79c9c28732f3c8d14f9600371159ff
Kabz Jose <josekabz00@gmail.com>
2025-09-22 18:00:41 +0300

    Code to fetch a joke

---
+++ fetch.js
@@ -0,0 +1,14 @@
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then(response => response.json()) // convert response to JSON
+  .then(data => console.log(data))   // use the data
+  .catch(error => console.log("Error:", error)); // handle errors
+
+  fetch("https://official-joke-api.appspot.com/random_joke")
+   .then(response => response.json()) 
+  .then(data=>{
+    console.log("Setup:", data.setup )
+    console.log("Punchline:", data.punchline)
+  })
+  .catch(error=> console.log("Error:",error))// Setup: Why don’t programmers like nature? Punchline: It has too many bugs.
+
+
