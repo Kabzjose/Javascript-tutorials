00c6aa14033a32b099a83d584443dd9705bb1472
Kabz Jose <josekabz00@gmail.com>
2025-09-23 00:30:56 +0300

    Async/await with setTimeout

--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-let boilRice=new Promise((resolve,reject)=>{
+/*let boilRice=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve("Rice is ready")
     },3000)
@@ -13,15 +13,15 @@ async function cook() {
 }
 console.log("washing dishes")// runs first because its outside the asyn function
 cook()
-
+*/
 async function getjoke() {
     try{
         console.log("Fetching a joke... ");
         let response=await fetch("https://official-joke-api.appspot.com/random_joke")
         let data=await response.json()
-         console.log("Data received:", data);  // check what you actually got
         console.log("Setup:",data.setup  || "No setup")
-        console.log("Punchline:",data.punchline || "No punchline")
+        await new Promise(resolve=>{ setTimeout(resolve ,3000) })
+       console.log("Punchline:",data.punchline || "No punchline")
     }catch (error){
    console.log("Error finding data:",error)//Error finding data: fetch failed
 
