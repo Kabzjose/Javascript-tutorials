e342e68c46cec731e6d22fa9999d6b646bf0a97b
Kabz Jose <josekabz00@gmail.com>
2025-09-22 23:49:39 +0300

    Async/await function

---
+++ async.js
@@ -0,0 +1,15 @@
+let boilRice=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Rice is ready")
+    },3000)
+})
+async function cook() {
+    console.log("start cooking")
+    let rice=await boilRice//waits here
+    console.log(rice)//runs after rice is ready
+    
+    console.log("Food is ready")
+    
+}
+console.log("washing dishes")// runs first because its outside the asyn function
+cook()
