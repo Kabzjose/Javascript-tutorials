aeeafc945f7f76c52aa0e7ae62bb84626119e748
Kabz Jose <josekabz00@gmail.com>
2025-09-29 23:37:14 +0300

    Added Try and catch to display any errors

--- objectloop.js
+++ objectloop.js
@@ -1,8 +1,13 @@
-const student ={
+//Added try and catch to display any error messages
+try{
+    const student ={
     name:"jose",
     age: 20,
     email:"Josekabz@gmail.com"
 }
 for(let key in student){
     console.log(key, student[key])
+}
+}catch(error){
+    console.log("something went wrong:",error.message)
 }
